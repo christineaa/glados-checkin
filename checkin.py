7f7489ec70f491950a8d231d629ddd27762def8c
Benature <wbenature@163.com>
2020-10-22 15:26:31 +0800

    恢复自动签到

--- checkin.py
+++ checkin.py
@@ -1,40 +1,51 @@
-import requests,json,os
+import requests
+import json
+import os
 
-# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-sever = os.environ["SERVE"]
-# 填写server酱sckey,不开启server酱则不用填
-sckey = os.environ["SCKEY"]
-#'SCU89402Tf98b7f01ca3394*********************************'
-# 填入glados账号对应cookie
-cookie = os.environ["COOKIE"]
-#'__cfduid=d3459ec306384ca67a65170f8e2a5bd************; _ga=GA1.2.766373509.1593*****72; _gid=GA1.2.1338236108.***********72; koa:sess=eyJ1c2VySW*********************aXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdl****0=; koa:sess.sig=6qG8SyMh*****LBc9yRviaPvI'
+# import environ
+# cookie = environ.cookie
+# wechat_bot = environ.wechat_bot
+cookie = os.environ["COOKIE"]  # 填入glados账号对应cookie
+wechat_bot = os.environ["WECHAT_BOT"]  # 企业微信机器人 url
 
 
+def wechat_bot_message(left_times, message):
+    headers = {"Content-Type": "text/plain"}
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "content": f'# Gloados\n' +
+                       f'当前剩余次数：<font color="warning">{left_times}</font>次\n' +
+                       f'>{message}'
+        }
+    }
+    r = requests.post(wechat_bot, headers=headers, json=data)
 
 
 def start():
-    
-    url= "https://glados.rocks/api/user/checkin"
-    url2= "https://glados.rocks/api/user/status"
-    referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-   # print(res)
+    payloadData = {"token": "glados_network"}
+
+    headers = {'cookie': cookie,
+               'referer': 'https://glados.rocks/console/checkin',
+               'origin': 'https://glados.network',
+               'content-type': 'application/json;charset=UTF-8',
+               }
+
+    url_checkin = "https://glados.rocks/api/user/checkin"
+    url_satus = "https://glados.rocks/api/user/status"
+
+    checkin = requests.post(url_checkin, headers=headers,
+                            data=json.dumps(payloadData))
+    state = requests.get(url_satus, headers=headers)
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
-        #print(time)
-        if sever == 'on':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
-    else:
-        requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
+        time = state.json()['data']['leftDays'].split('.')[0]
+        # print(time)
+        # print(mess)
+        wechat_bot_message(time, mess)
 
-def main_handler(event, context):
-  return start()
 
 if __name__ == '__main__':
+    # wechat_bot_message('0', 'Please Try Tomorrow')
     start()
-
-    
